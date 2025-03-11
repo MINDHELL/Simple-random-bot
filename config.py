ef4d441437e8eda5db45af935d6d8cd5824ce598
MINDHELL <Tgpurpose640@gmail.com>
2025-03-11 22:15:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = ""
+API_ID = "YOUR_API_ID"
+API_HASH = "YOUR_API_HASH"
+MONGO_URL = "YOUR_MONGODB_URL"
+CHANNEL_ID = -1001234567890  # Replace with your Telegram channel ID
+OWNER_ID = 123456789  # Replace with your Telegram user ID

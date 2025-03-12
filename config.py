e81ec141e62ab646c939d7c2e81ef98346c164a5
MINDHELL <Tgpurpose640@gmail.com>
2025-03-12 10:06:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://aarshhub:6L1PAPikOnAIHIRA@clus
 DATABASE_NAME = os.getenv("DATABASE_NAME", "Hd_content")  # MongoDB database name
 
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1002242458059"))  # Channel where videos are indexed
-FSUB_CHANNEL = os.getenv("FSUB_CHANNEL", "test89p")  # Force subscription channel
+FSUB_CHANNEL = os.getenv("FSUB_CHANNEL", "@test89p")  # Force subscription channel
 
 AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", "10"))  # Auto-delete sent videos (seconds)
 OWNER_ID = int(os.getenv("OWNER_ID", "6860316927"))  # Replace with your Telegram user ID
